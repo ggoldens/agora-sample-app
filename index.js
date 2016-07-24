2d453add4fdfe0a66d82d982e784bbb8557e1964
German <german.goldenstein@gmail.com>
2016-07-24 18:23:11 -0300

    Add ejs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
